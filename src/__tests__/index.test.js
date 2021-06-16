47f69023f09aa3d82118e968ed0bd0741fdfb265
hengshanMWC <1819293933@qq.com>
2021-06-16 14:44:42 +0800

    chore: 测试暂时停止

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,28 +1,17 @@
+// test.js
+
+// 从测试实用工具集中导入 `mount()` 方法
+// 同时导入你要测试的组件
 import { mount } from '@vue/test-utils'
-import classic from '@/components/demo/classic'
+import Counter from '../counter.js'
+require('jsdom-global')()
 
-describe('Component', () => {
-  test('is a Vue instance', () => {
-    const param = 4
+// 现在挂载组件，你便得到了这个包裹器
+const wrapper = mount(Counter)
 
-    const wrapper = mount(classic, {
-      propsData: {
-        param
-      }
-    })
-    async function handleStart () {
-      const b = await wrapper.handleStart()
-      toast(b)
-    }
-    function toast (b) {
-      if (b) {
-        const id = 17
-        const currentIndex = wrapper.currentIndex
-        const index = wrapper.items.findIndex(item => item.id === id)
-        expect(currentIndex).toBe(param)
-        expect(wrapper.$refs.prizes[index].isSelect).toBe(true)
-      }
-    }
-    handleStart()
-  })
-})
+// 你可以通过 `wrapper.vm` 访问实际的 Vue 实例
+const vm = wrapper.vm
+
+// 在控制台将其记录下来即可深度审阅包裹器
+// 我们对 Vue Test Utils 的探索也由此开始
+console.log(wrapper)
